b3b3bdbf57ef747a93a0503f7b34493441f517d8
Breno Oliveira Cavalcante <breno.cavalcante@ufu.br>
2024-01-04 15:54:31 -0300

    clientes

--- two/src/App.java
+++ two/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Programa 2!");
+        Cliente jorge = new Cliente();
+        jorge.printar();
     }
 }
